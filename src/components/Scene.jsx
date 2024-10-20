cba9736a28ad552aac82b9c4bf36f5b60b0f079f
Vishwas Singh <vishwass440@gmail.com>
2024-10-20 14:09:45 +0530

    added a 3d component

---
+++ src/components/Scene.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Sphere, Box, Torus } from '@react-three/drei';
+
+const Scene3D = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+
+      {/* A rotating torus */}
+      <Torus position={[-2, 0, 0]} args={[1, 0.4, 16, 100]} rotation={[0, 0.5, 0]}>
+        <meshStandardMaterial color="lightgreen" />
+      </Torus>
+
+      {/* A sphere */}
+      <Sphere position={[0, 0, 0]} args={[1, 32, 32]}>
+        <meshStandardMaterial color="lightblue" />
+      </Sphere>
+
+      {/* A simple box */}
+      <Box position={[2, 0, 0]} args={[1, 1, 1]}>
+        <meshStandardMaterial color="purple" />
+      </Box>
+
+      {/* Camera controls */}
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default Scene3D;
