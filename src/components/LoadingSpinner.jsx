fd603ef6cfa4ce83e96260e45a962158632d6b1e
Vishwas Singh <vishwass440@gmail.com>
2024-10-20 11:44:40 +0530

    fixed spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,20 +1,19 @@
 import React from "react"
 
-
-const LoadingSpinner =({size=40,color='dodgerblue'})=>{
-    return(
-      <div
+const LoadingSpinner = ({ size = 40, color = 'dodgerblue' }) => {
+  return (
+    <div
       className="loading-spinner"
       style={{
-        width:size,
-        height:size,
-        border:`4px solid ${color}`,
-        borderTop:`4px solid transparent`
+        width: size,
+        height: size,
+        border: `4px solid ${color}`,
+        borderTop: '4px solid transparent',
+        borderRadius: '50%'
       }}
-      >
-  
-      </div>
-    )
-  }
+    >
+    </div>
+  )
+}
 
 export default LoadingSpinner;
