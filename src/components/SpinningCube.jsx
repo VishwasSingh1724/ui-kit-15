54af40481ed696e541311a2530469c3800be90ec
Vishwas Singh <vishwass440@gmail.com>
2024-10-20 12:03:09 +0530

    added 3d spinner

---
+++ src/components/SpinningCube.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+const SpinningCube = ({ size = 1, color = 'hotpink', speed = 0.01 }) => {
+  // A simple cube that spins continuously
+  const Cube = () => {
+    const cubeRef = React.useRef();
+    
+    React.useFrame(() => {
+      if (cubeRef.current) {
+        cubeRef.current.rotation.x += speed;
+        cubeRef.current.rotation.y += speed;
+      }
+    });
+
+    return (
+      <mesh ref={cubeRef}>
+        <boxGeometry args={[size, size, size]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    );
+  };
+
+  return (
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} intensity={0.8} />
+      <Cube />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
+};
+
+export default SpinningCube;
